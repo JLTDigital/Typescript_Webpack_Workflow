ab0a118cb90128f0fa8af37d37c24e331788076e
JLTDigital <mrjltaft@gmail.com>
2021-05-27 21:57:40 +0100

    Init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Typescript & Webpack Workflow
+
+Webpack configuration to use Typescript
+
+## Usage
+
+**Installation**
+
+Install the dependancies
+
+```$ npm install```
+
+**Run Webpack Dev Server**
+
+To run both server - Runs on localhost: 8080
+
+```$ npm run dev```
+
+**Build**
+
+Builds a bundle.js build in the build folder. Must be done before serving
+
+```$ npm run build```
+
+# More Info
+
+**Author**
+
+Jonathan Taft
+
+**Licence**
+
+ISC
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+
+module.exports = {
+  devtool: 'source-map',
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'build')
+  }
+}
